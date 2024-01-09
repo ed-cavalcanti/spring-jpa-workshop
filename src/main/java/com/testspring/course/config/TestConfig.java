7348d887012259da8460ef4fadafc773ffc1aa58
ed-cavalcanti <edney2809@gmail.com>
2024-01-08 22:32:53 -0300

    [feat] OrderStatus enum

--- src/main/java/com/testspring/course/config/TestConfig.java
+++ src/main/java/com/testspring/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.testspring.course.entities.Order;
 import com.testspring.course.entities.User;
+import com.testspring.course.entities.enums.OrderStatus;
 import com.testspring.course.repositories.OrderRepository;
 import com.testspring.course.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User user1 = new User(null, "Jon Snow", "jon@gmail.com", "999999999", "123456");
         User user2 = new User(null, "Eddard Stark", "edstark@gmail.com", "988888888", "123456");
 
-        Order or1 = new Order(null, Instant.parse("2024-01-06T19:53:07Z"), user1);
-        Order or2 = new Order(null, Instant.parse("2024-01-02T03:42:10Z"), user2);
-        Order or3 = new Order(null, Instant.parse("2024-01-06T15:21:22Z"), user1);
+        Order or1 = new Order(null, Instant.parse("2024-01-06T19:53:07Z"), OrderStatus.PAID, user1);
+        Order or2 = new Order(null, Instant.parse("2024-01-02T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
+        Order or3 = new Order(null, Instant.parse("2024-01-06T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
 
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(or1, or2, or3));
