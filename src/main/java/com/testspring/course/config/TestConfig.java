4cd52faf68f46ea98005ed3451390c5ed393fe4f
ed-cavalcanti <edney2809@gmail.com>
2024-01-09 00:48:40 -0300

    [feat] Payment Entity, one-to-one association

--- src/main/java/com/testspring/course/config/TestConfig.java
+++ src/main/java/com/testspring/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.testspring.course.entities.Category;
 import com.testspring.course.entities.Order;
 import com.testspring.course.entities.OrderItem;
+import com.testspring.course.entities.Payment;
 import com.testspring.course.entities.Product;
 import com.testspring.course.entities.User;
 import com.testspring.course.entities.enums.OrderStatus;
@@ -78,6 +79,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(or3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2024-01-06T21:53:07Z"), or1);
+        or1.setPayment(pay1);
+
+        orderRepository.save(or1);
     }
 
 }
