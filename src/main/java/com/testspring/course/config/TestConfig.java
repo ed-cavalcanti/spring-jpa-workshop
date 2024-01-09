6151ebcc2c5f167264fff548864b8f9e802814f0
ed-cavalcanti <edney2809@gmail.com>
2024-01-08 23:21:56 -0300

    [feat] Product Entity

--- src/main/java/com/testspring/course/config/TestConfig.java
+++ src/main/java/com/testspring/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.testspring.course.entities.Category;
 import com.testspring.course.entities.Order;
+import com.testspring.course.entities.Product;
 import com.testspring.course.entities.User;
 import com.testspring.course.entities.enums.OrderStatus;
 import com.testspring.course.repositories.CategoryRepository;
 import com.testspring.course.repositories.OrderRepository;
+import com.testspring.course.repositories.ProductRepository;
 import com.testspring.course.repositories.UserRepository;
 
 @Configuration
@@ -29,13 +31,23 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public void run(String... args) throws Exception {
         Category cat1 = new Category(null, "Armors");
         Category cat2 = new Category(null, "Swords");
         Category cat3 = new Category(null, "Mount");
 
+        Product p1 = new Product(null, "Longsword", "Longsword description", 199.99, "");
+        Product p2 = new Product(null, "Leather Boots", "Boots description", 45.00, "");
+        Product p3 = new Product(null, "Horse", "Horse description", 2000.00, "");
+        Product p4 = new Product(null, "Iron Helmet", "Iron Helmet description", 68.50, "");
+        Product p5 = new Product(null, "Ice sword", "Ice sword description", 459.90, "");
+
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
         User user1 = new User(null, "Jon Snow", "jon@gmail.com", "999999999", "123456");
         User user2 = new User(null, "Eddard Stark", "edstark@gmail.com", "988888888", "123456");
